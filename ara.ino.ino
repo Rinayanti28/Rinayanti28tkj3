208e9e2f995e1b9580b7f340b8b77fd4e8fdc562
Rinayanti28 <rinayan28@gmail.com>
2016-12-05 21:03:52 +0700

    Add files via upload

---
+++ ara.ino.ino
@@ -0,0 +1,23 @@
+Light Dependent Resistor= A2;          // membuat variabel LDR untuk pin A2
+int LED= 13;          // membuat variabel LED untuk pin 13
+int nilaiLDR= 0;  // variabel nilai awal untuk nilaiLDR
+void setup() {
+  // put your setup code here, to run once:
+pinMode(LED, OUTPUT);          // menentukan LED menjadi OUTPUT
+Serial.begin(9600);          // komunikasi Arduino ke Komputer
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+nilaiLDR= analogRead(LDR);   // menyimpan nilai yang dibaca dari LDR ke variabel nilaiLDR
+  Serial.print("NilaiLDR= ");         // menampilkan teks nilaiLDR=
+  Serial.println(nilaiLDR);                 // menampilkan nilai dari nilaiLDR
+  if(nilaiLDR < 500) {          // jika nilai dari LDR kurang dari 500
+    digitalWrite(LED, HIGH);         // lampu LED menyala
+  }
+  else  {                           // jika tidak
+    digitalWrite(LED, LOW);         // lampu LED mati
+  }
+
+}
